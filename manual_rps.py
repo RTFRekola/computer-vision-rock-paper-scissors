254f3f5c5cbda9d9b9e9d6ccd96443e1b109521e
RTFRekola <rtfrekola@gmail.com>
2023-02-20 19:10:09 +0000

    Chooses the winner

--- manual_rps.py
+++ manual_rps.py
@@ -9,3 +9,11 @@ def get_user_choice():
     user_choice = input("Rock, paper or scissors? ")
     return user_choice
 
+def get_winner(computer_choice, user_choice):
+    if computer_choice==user_choice:
+        print("It is a tie!")
+    elif computer_choice=="rock" and user_choice=="scissors" or computer_choice=="scissors" and user_choice=="paper" or computer_choice=="paper" and user_choice=="rock":
+        print("You lost")
+    else:
+        print("You won!")
+    # end if
