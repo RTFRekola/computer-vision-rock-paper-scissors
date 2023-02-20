e8c88e7a970a6a973aa73390c93f6b15e81b762c
RTFRekola <rtfrekola@gmail.com>
2023-02-20 19:28:40 +0000

    Creates the camera version with a function to analyse image

--- camera_rps.py
+++ camera_rps.py
@@ -1,7 +1,8 @@
 import cv2
+import numpy as np
 
 def get_prediction(model):
-    model_index = numpy.argmax(model)
+    model_index = np.argmax(model)
     if model_index==0:
         choice = "nothing"
     elif model_index==1:
