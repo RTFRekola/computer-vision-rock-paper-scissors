edad4b0da9af50fe309c41f5078a40db0e59b53c
RTFRekola <rtfrekola@gmail.com>
2023-02-20 19:25:31 +0000

    Creates the camera version with a function to analyse image

---
+++ camera_rps.py
@@ -0,0 +1,12 @@
+def get_prediction(model):
+    model_index = numpy.argmax(model)
+    if model_index==0:
+        choice = "nothing"
+    elif model_index==1:
+        choice = "rock"
+    elif model_index==2:
+        choice = "paper"
+    else:
+        choice = "scissors"
+    # end if
+    return choice
